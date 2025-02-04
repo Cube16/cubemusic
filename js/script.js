47f859cf515640810c3667bdefa986d388191abe
Cube <112111860+Cube16@users.noreply.github.com>
2025-02-04 16:18:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function filterReleases() {
+		const selectedCategory = document.querySelector('input[name="release-category"]:checked').value;
+		const musicCards = document.querySelectorAll('.music-card');
+
+		musicCards.forEach(card => {
+			const categories = card.getAttribute('data-category').split(' ');
+			if (selectedCategory === 'All' || categories.includes(selectedCategory)) {
+            card.style.display = 'block';
+			} else {
+            card.style.display = 'none';
+			}
+		});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    filterReleases(); // Фильтруем при загрузке страницы
+		const filters = document.querySelectorAll('input[name="release-category"]');
+		filters.forEach(filter => {
+			filter.addEventListener('change', filterReleases);
+		});
+});
